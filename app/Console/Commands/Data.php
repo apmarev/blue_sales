a72ede93a69ce0b96e54959b1b99bbdc9837e61b
apmarev <57302841+apmarev@users.noreply.github.com>
2022-10-25 14:29:32 +0200

    Start process

--- app/Console/Commands/Data.php
+++ app/Console/Commands/Data.php
@@ -26,41 +26,37 @@ class Data extends Command {
         $controllerBlueSales = new BlueSalesController();
         $controllerAmoCrmSend = new AmoCrmSendController();
 
-        $controllerImport = new ImportController();
+        Export::truncate();
+        Clients::truncate();
+        Orders::truncate();
 
-        // $controllerImport->get();
+        $controllerAmoCrm->contactsPull(); // Скачивание всех контактов из АмоСрм
+        $controllerBlueSales->getClients(); // Скачивание клиентов из BlueSales за вчерашний день
+        $controllerBlueSales->getOrdersYesterday(); // Скачивание всех заказов из BlueSales
 
-//        Export::truncate();
-//        Clients::truncate();
-//        Orders::truncate();
+        LogProvider::log("Загрузка данных из AmoCRM и BlueSales завершена");
 
-         // $controllerAmoCrm->contactsPull(); // Скачивание всех контактов из АмоСрм
-//         $controllerBlueSales->getClients(); // Скачивание клиентов из BlueSales за вчерашний день
-//         $controllerBlueSales->getOrdersYesterday(); // Скачивание всех заказов из BlueSales
-//
-        //LogProvider::log("Загрузка данных из AmoCRM и BlueSales завершена");
-//
-//         $controllerBlueSales->getAndRemoveClients(); // Удаление дубляжей клиентов
-//        LogProvider::log("Дубликаты удалены");
-//
-//         $controllerBlueSales->checkClientsFromBS(); // Подготовка данных клиентов к экспорту в амо
-//          $controllerBlueSales->checkOrdersFromBS(); // Подготовка данных заказов к экспорту в амо
-//
-//        LogProvider::log("Данные к экспорту в AmoCrm подготовлены");
-//
+        $controllerBlueSales->getAndRemoveClients(); // Удаление дубляжей клиентов
+        LogProvider::log("Дубликаты удалены");
+
+        $controllerBlueSales->checkClientsFromBS(); // Подготовка данных клиентов к экспорту в амо
+        $controllerBlueSales->checkOrdersFromBS(); // Подготовка данных заказов к экспорту в амо
+
+        LogProvider::log("Данные к экспорту в AmoCrm подготовлены");
+        //
         $count = Export::where('type', 'lead')->where('action', 'create')->count();
         $controllerAmoCrmSend->getCreateLeads(); // Экспорт новых сделок в Амо
         LogProvider::log("Создано сделок: {$count}");
-//
-//        $count = Export::where('type', 'lead')->where('action', 'update')->count();
-//         $controllerAmoCrmSend->getUpdateLeads(); // Экспорт обновленных сделок в Амо
-//        LogProvider::log("Изменено сделок: {$count}");
-//
-//        $count = Export::where('type', 'all')->where('action', 'create')->count();
-//         $controllerAmoCrmSend->getCreateLeadsAndContacts(); // Экспорт сделок с контактами в Амо
-//        LogProvider::log("Создано сделок вместе с новыми контактами: {$count}");
-//
-//        LogProvider::log("Операция завершена");
+
+        $count = Export::where('type', 'lead')->where('action', 'update')->count();
+        $controllerAmoCrmSend->getUpdateLeads(); // Экспорт обновленных сделок в Амо
+        LogProvider::log("Изменено сделок: {$count}");
+
+        $count = Export::where('type', 'all')->where('action', 'create')->count();
+        $controllerAmoCrmSend->getCreateLeadsAndContacts(); // Экспорт сделок с контактами в Амо
+        LogProvider::log("Создано сделок вместе с новыми контактами: {$count}");
+
+        LogProvider::log("Операция завершена");
 
         return "Success";
     }
